fd981bf2ccf88664226624434b430b91ef00045a
Phobian99 <phobianwafula99>
2023-02-16 03:53:32 -0800

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main-main entry
+ *main - main entry
  *
  *return: always return 0
  */
 int main(void) 
 {
-printf("with proper grammar, but the outcome is a piece of art,);
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
