3cb5305befe58e39f7562589fc78a54049091778
Phobian99 <phobianwafula99>
2023-02-17 09:33:39 -0800

    if,while variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints all possible different combinations of two digits
+ * main - Program that prints all possible different combinations of two digits
  * Return: 0 (Success)
  */
 int main(void)
