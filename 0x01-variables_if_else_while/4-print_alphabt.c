8694ab09d8e551329517c2a26e54d0d4eafa8e55
Phobian99 <phobianwafula99>
2023-02-17 08:26:59 -0800

    if,while variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0(succes)
+ */
+int main(void)
+
+{
+        char Y;
+
+        for (Y = 'a'; Y <= 'z'; Y++)
+        {
+                if (Y != 'q'&&Y != 'e')
+        putchar(Y);
+        }
+        putchar('\n');
+        return (0);
+}
