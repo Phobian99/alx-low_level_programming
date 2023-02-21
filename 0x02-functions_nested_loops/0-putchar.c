c95073fb8b047dcebdf7e37443079d3b73db6f5d
Phobian99 <phobianwafula99>
2023-02-21 04:13:01 -0800

    if,while variables

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *main - Program that prints _putchar
- *
- *Return: 0 (Success)
+ * main - Program that prints _putchar
+ * followed by a new line
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	putchar('_');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main entry of the program
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void);
+{
+	char g;
+	
+	putchar(g = 'a'; g <= z; g++)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+
+	return(0);
+}
