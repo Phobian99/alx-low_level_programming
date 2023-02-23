4e9bf97af058521b1e0f12fe08d816edb8fd4328
Phobian99 <phobianwafula99>
2023-02-23 03:45:12 -0800

    iaf

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 /**
  *_isdigit -function that checks for a digit
  *
@@ -6,10 +6,8 @@
  */
 int _isdigit(int c)
 {
-	if(c >= '0' && c <= '9')
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
 	else
 		return (0);
 }
