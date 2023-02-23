1ba86755673b49f2d6dd7bdfdbf06eb1cb900f3b
Phobian99 <phobianwafula99>
2023-02-23 06:29:09 -0800

    iaf

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,48 +1,31 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * print_times_table - print the time tables
- *@n: the tested number
- *
- * Return: the time tables from 0 to n
- */
-void print_times_table(int n)
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+
+int main(void)
 {
-	int multi, row, col, cents, tens, units;
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
 
-	for (row = 0; row <= n; row++)
-	{
-		for (col = 0; col <= n; col++)
-		{
-			if (n <= 15 && n >= 0)
-			{
-				multi = row * col;
-				cents = multi / 100;
-				tens = (multi / 10) % 10;
-				units = multi % 10;
-				if (col == 0)
-				{
-					_putchar('0'); }
-				else
-				{
-					if (multi > 99)
-						_putchar(cents + '0');
-					else
-						_putchar(' ');
-					if (multi > 9)
-						_putchar(tens + '0');
-					else
-						_putchar(' ');
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
 
-					_putchar(units + '0');
-				}
-				if (col < n)
-				{
-					_putchar(',');
-					_putchar(' '); }
-				else
-				{
-					_putchar('\n'); }
-			}
-		}
-	}
+}
+}
+printf("%ld\n", number);
+return (0);
 }
