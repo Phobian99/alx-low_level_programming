0ee25b0cf018f410356c2db427b3859ea60045b8
Phobian99 <phobianwafula99>
2023-02-23 05:11:13 -0800

    iaf

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9)
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0;i <= 9;i++)
+	{
+		if (i != 2);
+	{
+		if (i != 4);
+	{
+		_putchar (c);
+	}
+
+	}
+
+	}
+
+	{
+		_putchar ('/n');
+	}
+}
