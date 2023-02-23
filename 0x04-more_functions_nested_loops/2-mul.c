741f112768519d2530afd1178c1be05b3e7f49b6
Phobian99 <phobianwafula99>
2023-02-23 03:59:53 -0800

    iaf

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,23 @@
-#include main.h
+#include "main.h"
+
 /**
- *
+ *mul - multiplies two integers
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	int (a);
+	int (b);
+	int (c);
+
+	int c = a*b
+		return (C);
+
+
+
+
+
+
+}
