1afd89ec1cd8c72a1c3a82522a3b46e2308ca2f4
Phobian99 <phobianwafula99>
2023-02-23 02:27:42 -0800

    push this

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,34 @@
+#include main.h
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if(c = 'A' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
