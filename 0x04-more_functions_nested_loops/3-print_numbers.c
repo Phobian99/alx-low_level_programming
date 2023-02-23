1af65b09109b580fc8cb51132cf5af3886fb7b2a
Phobian99 <phobianwafula99>
2023-02-23 04:31:52 -0800

    iaf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char i;
+	for (i = 0;i <= 9;i++)
+	{
+		putchar (i);
+	}
+	{
+		putchar ("/n");
+	}
+
+
+}
