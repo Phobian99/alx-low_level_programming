9485fa4dbd9ef8b9fbebe20d0a7086db1d9a051e
Phobian99 <phobianwafula99>
2023-02-27 03:39:30 -0800

    strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+    while (s[len] != '\0') len++;
+    return len;
+
+
+
+
+
+
+
+
+
+}
