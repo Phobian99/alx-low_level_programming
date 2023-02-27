2ff02daf41815a4352b128aad328788d7332cbf7
Phobian99 <phobianwafula99>
2023-02-27 03:16:58 -0800

    strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - a function that swaps the values of two integers.
+ * @c: input
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+    *a = *b;
+    *b = c;
+
+
+
+
+
+}
