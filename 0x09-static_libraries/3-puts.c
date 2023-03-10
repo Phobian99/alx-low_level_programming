8e2a97f0ff613e34a1180dd996678166dad313fa
Phobian99 <phobianwafula99>
2023-03-10 09:46:23 -0800

    yes

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts(s);
 }
